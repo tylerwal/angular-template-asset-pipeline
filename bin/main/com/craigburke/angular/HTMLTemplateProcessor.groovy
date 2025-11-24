2aefd7376b0381e2dcaead9fc2ec14b9726eb092
Tyler Waldron <twaldron@horus.net>
2025-11-24 09:22:46 -0500

    rules lib

---
+++ bin/main/com/craigburke/angular/HTMLTemplateProcessor.groovy
@@ -0,0 +1,27 @@
+package com.craigburke.angular
+
+import asset.pipeline.AssetCompiler
+import asset.pipeline.AssetPipelineConfigHolder
+import asset.pipeline.AssetFile
+import static com.craigburke.angular.TemplateProcessorUtil.*
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class HTMLTemplateProcessor {
+
+    HTMLTemplateProcessor(AssetCompiler precompiler) {}
+
+    def process(String input, AssetFile assetFile) {
+        Map assetConfig = (Map)AssetPipelineConfigHolder.config?.angular ?: [:]
+        ProcessorConfig config = new ProcessorConfig(assetConfig)
+
+        String moduleName = getModuleName(assetFile, config)
+
+        String templateName = getTemplateName(assetFile, config.templateFolder, config.includePathInName)
+        String content = formatHtml(input.toString(), config)
+
+        getTemplateJs(moduleName, templateName, content)
+    }
+
+}
